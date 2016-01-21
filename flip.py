733e6ffc292af9184016178ce4c69e050c371ab0
dgd <david.dicarlo@nxp.com>
2016-01-21 11:46:17 -0600

    added gain and exposure adjustments, dump of v4l2 info

--- flip.py
+++ flip.py
@@ -42,22 +42,43 @@ def on_key_release (widget, eventkey):
   if eventkey.string=='t':
       # lock camera settings...
       print 'Lock camera settings...'
-      os.system('v4l2-ctl -d /dev/video1 -C white_balance_temperature')
-      os.system('v4l2-ctl -d /dev/video1 -c focus_auto=0 -c exposure_auto=1 -c  white_balance_temperature_auto=0')
-      os.system('v4l2-ctl -d /dev/video1 -C white_balance_temperature')
+      os.system('v4l2-ctl -d /dev/video1 -c focus_auto=0,exposure_auto=1,white_balance_temperature_auto=0,exposure_auto_priority=0')
   if eventkey.string=='u':
       # unlock camera settings...
       print 'Unlock camera settings...'
-      os.system('v4l2-ctl -d /dev/video1 -C white_balance_temperature')
-      os.system('v4l2-ctl -d /dev/video1 -c focus_auto=1 -c exposure_auto=3 -c  white_balance_temperature_auto=1')
-      os.system('v4l2-ctl -d /dev/video1 -C white_balance_temperature')
+      os.system('v4l2-ctl -d /dev/video1 -c focus_auto=1,exposure_auto=3,white_balance_temperature_auto=1,exposure_auto_priority=1')
   if eventkey.string in ['y','v']:
       a, wb = commands.getstatusoutput('v4l2-ctl -d /dev/video1 -C  white_balance_temperature')
       wb = eval(split(wb, ':')[1])
       if eventkey.string=='y': wb +=20
       if eventkey.string=='v': wb -=20
-      os.system('v4l2-ctl -d /dev/video2 -c white_balance_temperature='+str(wb))
-      print wb
+      if wb<2000: wb=2000
+      if wb>6500: wb=6500
+      os.system('v4l2-ctl -d /dev/video1 -c white_balance_temperature='+str(wb))
+      print 'New white balance:', wb
+  if eventkey.string == 'D':
+      os.system('v4l2-ctl -d /dev/video1  -C brightness -C contrast -C saturation -C white_balance_temperature_auto -C gain -C power_line_frequency -C white_balance_temperature -C sharpness -C backlight_compensation -C exposure_auto -C exposure_absolute -C exposure_auto_priority -C pan_absolute -C tilt_absolute -C focus_absolute -C focus_auto -C zoom_absolute -C led1_mode -C led1_frequency')
+  if eventkey.string == 'd':
+      os.system('v4l2-ctl -d /dev/video1  -C white_balance_temperature_auto -C gain -C white_balance_temperature -C exposure_auto -C exposure_absolute -C focus_absolute -C focus_auto')
+  if eventkey.string in ['g','G']:
+      a, wb = commands.getstatusoutput('v4l2-ctl -d /dev/video1 -C  gain')
+      wb = eval(split(wb, ':')[1])
+      if eventkey.string=='G': wb +=5
+      if eventkey.string=='g': wb -=5
+      if wb<0: wb=0
+      if wb>255: wb=255
+      os.system('v4l2-ctl -d /dev/video1 -c gain='+str(wb))
+      print 'New gain:', wb
+  if eventkey.string in ['e','E']:
+      a, wb = commands.getstatusoutput('v4l2-ctl -d /dev/video1 -C  exposure_absolute')
+      wb = eval(split(wb, ':')[1])
+      if eventkey.string=='E': wb +=20
+      if eventkey.string=='e': wb -=20
+      if wb<3: wb=3
+      if wb>2047: wb=2047
+      os.system('v4l2-ctl -d /dev/video1 -c exposure_absolute='+str(wb))
+      print 'New exposure:', wb
+
 
 if __name__=="__main__":
     Gdk.init([])
